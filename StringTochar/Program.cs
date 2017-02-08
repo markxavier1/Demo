c100a6defb2226a3fe8b74e6d453df5e2f2092a1
markxavier1 <markxavier1@outlook.com>
2017-02-08 11:07:51 +0000

    New update 2

---
+++ StringTochar/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StringTochar
+{
+    class Program
+    {
+        //string val= "07/02/2017 11:00:00 AM";
+       
+        static void Main(string[] args)
+        {
+           
+        }
+    }
+
+
+   public class aa
+    {
+        public void s()
+        {
+
+            string val = "07/02/2017 11:00:00 AM";
+            string array = val.Substring(0, 10);
+        }
+    }
+
+
+}
